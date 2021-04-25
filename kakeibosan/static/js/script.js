71feb3d9ee12778c11d067ef69a2acba08daea57
showgayaki <knk.lee.kj@gmail.com>
2021-04-25 14:49:12 +0900

    ローディングアニメーション表示調整

--- kakeibosan/static/js/script.js
+++ kakeibosan/static/js/script.js
@@ -1,5 +1,9 @@
+// window読み込み終了時にローディングアニメーション終了
+$(window).on('load', function(){
+    hideLoader();
+})
 // ログイン時にローディングアニメーション表示
-$('.form-signin').submit( function(){
+$('.form-signin').submit(function(){
     displayLoader();
 });
 // レコード追加時にローディングアニメーション表示
@@ -8,7 +12,14 @@ $(document).on('click', '.to-loading', function(){
     displayLoader();
 });
 
+// ローディングアニメーション表示
 function displayLoader(){
     $('.loader').css('display', 'block');
     $('.loader-bg').css('display', 'block');
+}
+
+// ローディングアニメーション非表示
+function hideLoader(){
+    $('.loader').css('display', 'none');
+    $('.loader-bg').css('display', 'none');
 }

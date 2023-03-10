a9c21d535aeb9b51ba9f8d52ef76de7d39e3df3d
showgayaki <knk.lee.kj@gmail.com>
2023-03-10 16:00:26 +0900

    ダッシュボードの関数名など修正

--- kakeibosan/views/dashboard.py
+++ kakeibosan/views/dashboard.py
@@ -16,28 +16,33 @@ def dashboard():
     this_month = datetime.today().date().replace(day=1)
     _insert_fixed_costs(this_month)
 
-    sub_categories = ('電気代', 'ガス代', '水道代')
-    total_per_months = _total_per_months(this_month)
-
     category = ('固定費', '光熱費', '食費', '日用品', '交通費')
-    costs_per_month = _costs_per_month(this_month, category)
+    sub_categories = ('電気代', 'ガス代', '水道代')
 
-    utility_costs = _utility_per_month(sub_categories, total_per_months['months'])
+    total_costs_last_12_months = _total_costs_last_12_months(this_month)
+    costs_this_month_by_category = _costs_this_month_by_category(this_month, category)
+    utility_costs = _utility_costs_last_12_months(sub_categories, total_costs_last_12_months['months'])
 
-    # グラフ用に配列を逆順にする->[::-1]
-    total_per_months['total'] = total_per_months['total'][::-1]
-    total_per_months['months'] = [month.strftime('%-m月') for month in total_per_months['months']][::-1]
+    # 「○月」の形にする
+    total_costs_last_12_months['months'] = [month.strftime('%-m月') for month in total_costs_last_12_months['months']]
 
-    return render_template('dashboard.html', active_page='ダッシュボード', this_month=this_month,
-                           total_per_months=total_per_months, costs_per_month=costs_per_month,
-                           utility_costs=utility_costs)
+    return render_template(
+        'dashboard.html',
+        active_page='ダッシュボード',
+        this_month=this_month,
+        total_costs_last_12_months=total_costs_last_12_months,
+        costs_this_month_by_category=costs_this_month_by_category,
+        utility_costs=utility_costs
+    )
 
 
-def _total_per_months(this_month):
-    month_count = 12
+def _total_costs_last_12_months(this_month):
+    # 直近12ヶ月の支出合計（ページ上部の折れ線グラフ用）
+    MONTH_COUNT = 12
     last_12_months = []
-    total_costs = []
-    for i in range(month_count):
+    total_costs_per_month = []
+    # 今月分からひと月ごとに遡って、直近12ヶ月分を合計していく
+    for i in range(MONTH_COUNT):
         month = (this_month + relativedelta(months=-i))
         last_12_months.append(month)
         try:
@@ -46,42 +51,46 @@ def _total_per_months(this_month):
             costs = {}
         finally:
             db.session.close()
-        total = 0
-        total += sum(cost.amount for cost in costs)
-        total_costs.append(total)
 
-    total_dict = {'months': last_12_months, 'total': total_costs}
+        total = sum(cost.amount for cost in costs)
+        total_costs_per_month.append(total)
+
+    # 降順に並べ替えて返す
+    total_dict = {'months': last_12_months[::-1], 'total': total_costs_per_month[::-1]}
     return total_dict
 
 
-def _costs_per_month(this_month, category):
-    cost_per_month = []
+def _costs_this_month_by_category(this_month, category):
+    # 今月のカテゴリー別支出（円グラフ用）
+    costs_by_category = []
     for cat in category:
         try:
             costs = Cost.query.filter(and_(Cost.month_to_add == this_month, Cost.category == cat)).all()
             total_dict = {'category': cat, 'amount': sum(cost.amount for cost in costs)}
-            cost_per_month.append(total_dict)
+            costs_by_category.append(total_dict)
         except AttributeError:
-            cost_per_month[cat] = 0
+            costs_by_category[cat] = 0
         finally:
             db.session.close()
-    return cost_per_month
+
+    return costs_by_category
 
 
-def _utility_per_month(sub_categories, months):
+def _utility_costs_last_12_months(sub_categories, months):
+    # 直近12か月分の水道光熱費（各カードの折れ線ブラフ用）
     utility_costs = {}
     for month in months:
-        utility_charnge = {}
+        utility_costs_per_month = {}
         for sc in sub_categories:
             try:
                 cost = Cost.query.filter(
                     and_(Cost.sub_category == sc, Cost.month_to_add == month)).order_by(Cost.id).first()
-                utility_charnge[sc] = cost.amount
+                utility_costs_per_month[sc] = cost.amount
             except AttributeError:
-                utility_charnge[sc] = 0
+                utility_costs_per_month[sc] = 0
             finally:
                 db.session.close()
-        utility_costs['{0:%Y-%m}'.format(month)] = utility_charnge
+        utility_costs['{0:%Y-%m}'.format(month)] = utility_costs_per_month
 
     return utility_costs
 

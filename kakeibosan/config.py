9311c2749828d700bca6d77fffde12fa38000a66
showgayaki <knk.lee.kj@gmail.com>
2023-05-24 22:48:01 +0900

    DBをMariaDBに変更

--- kakeibosan/config.py
+++ kakeibosan/config.py
@@ -1,13 +1,24 @@
 import os
+from dotenv import load_dotenv
+from pathlib import Path
 
 
-class DevelopmentConfig:
+class SystemConfig:
+    root_dir = Path(__file__).resolve().parents[1]
+    dotenv_path = Path(root_dir).joinpath('.env')
+    load_dotenv(dotenv_path)
+
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' \
-                              + os.path.join(os.path.abspath(os.path.dirname(__file__)), 'models/kakeibosan.db')
-    SECRET_KEY = '\xec\x0e\x0f\x87\x83I\x87\x0b\xef\xfb\x15\xb8\xeb\xeb\x1e\x87M'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{user}:{password}@{host}/{db_name}?charset=utf8'.format(**{
+        'user': os.environ.get('DB_USER'),
+        'password': os.environ.get('DB_PASS'),
+        'host': os.environ.get('DB_HOST'),
+        'db_name': os.environ.get('DB_NAME'),
+    })
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = False
 
 
-config = DevelopmentConfig
+config = SystemConfig

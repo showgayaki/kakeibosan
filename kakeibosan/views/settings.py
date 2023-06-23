811d81d067f6b225041981a9871b6e5920d2790a
showgayaki <knk.lee.kj@gmail.com>
2023-06-23 10:47:51 +0900

    設定ページにカテゴリー一覧追加

--- kakeibosan/views/settings.py
+++ kakeibosan/views/settings.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_required
 from kakeibosan import db
 from kakeibosan.models import User, FixedCost, Category, CategoryPaths
@@ -11,16 +11,30 @@ bp = Blueprint('settings', __name__)
 @login_required
 def settings():
     if request.method == 'POST':
-        parameter = request.args.get('card')
-        if parameter == 'add_account':
-            return redirect(url_for('edit_account.edit_account', edit='add'))
-        elif parameter == 'fixedcost':
-            return redirect(url_for('edit_fixedcost.edit_fixedcost', edit='add'))
+        flash_category = 'warning'
+        flash_message = f'このページでは【{request.method}】は受け付けられません'
+        flash(flash_message, flash_category)
+        return redirect(url_for('settings.settings'))
     else:
         category_ancestor = db.orm.aliased(Category)
         category_descendant = db.orm.aliased(Category)
         try:
+            # ユーザーアカウント用
             users = db.session.query(User).order_by(User.id).all()
+
+            # カテゴリー用
+            category = db.session.query(
+                Category.id,
+                category_ancestor.name.label('category'),
+                category_descendant.name.label('subcategory'),
+                Category.in_english,
+                Category.chart_color,
+            ).join(category_descendant, Category.id == category_descendant.id)\
+             .join(CategoryPaths, Category.id == CategoryPaths.descendant)\
+             .join(category_ancestor, CategoryPaths.ancestor == category_ancestor.id)\
+             .all()
+
+            # 固定費用
             fixed_costs = db.session.query(
                 FixedCost.id,
                 category_ancestor.name.label('category'),
@@ -32,8 +46,10 @@ def settings():
              .join(CategoryPaths, FixedCost.category_id == CategoryPaths.descendant)\
              .join(category_ancestor, CategoryPaths.ancestor == category_ancestor.id)\
              .all()
-        except db.exc.SQLAlchemyError:
+        except db.exc.SQLAlchemyError as e:
+            print(e)
             users = []
+            category = []
             fixed_costs = []
         finally:
             db.session.close()
@@ -41,6 +57,7 @@ def settings():
         return render_template(
             'settings.html',
             active_page='設定',
-            fixed_costs=fixed_costs,
             users=users,
+            category=category,
+            fixed_costs=fixed_costs,
         )

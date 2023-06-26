43560d9c269339d90d9d82f49eb707f4e980b896
showgayaki <knk.lee.kj@gmail.com>
2023-06-26 10:29:58 +0900

    categoryテーブルのid見直し

--- migrations/versions/bec9f21b662a_.py
+++ migrations/versions/bec9f21b662a_.py
@@ -23,7 +23,7 @@ def upgrade():
     op.execute('DROP TABLE IF EXISTS category')
 
     category = op.create_table('category',
-                               sa.Column('id', sa.Integer(), autoincrement=True, nullable=False),
+                               sa.Column('id', sa.Integer(), autoincrement=False, nullable=False),
                                sa.Column('name', sa.String(length=100), nullable=False),
                                sa.Column('in_english', sa.String(length=100), nullable=True),
                                sa.Column('chart_color', sa.String(length=100), nullable=True),
@@ -40,59 +40,59 @@ def upgrade():
     op.bulk_insert(category,
                    [
                        # 固定費
-                       {'name': '固定費', 'in_english': None, 'chart_color': None},
-                       {'name': '家賃', 'in_english': None, 'chart_color': None},
-                       {'name': '管理費', 'in_english': None, 'chart_color': None},
-                       {'name': '手数料', 'in_english': None, 'chart_color': None},
-                       {'name': '更新料', 'in_english': None, 'chart_color': None},
-                       {'name': '駐輪場', 'in_english': None, 'chart_color': None},
+                       {'id': 100, 'name': '固定費', 'in_english': None, 'chart_color': None},
+                       {'id': 101, 'name': '家賃', 'in_english': None, 'chart_color': None},
+                       {'id': 102, 'name': '管理費', 'in_english': None, 'chart_color': None},
+                       {'id': 103, 'name': '手数料', 'in_english': None, 'chart_color': None},
+                       {'id': 104, 'name': '更新料', 'in_english': None, 'chart_color': None},
+                       {'id': 105, 'name': '駐輪場', 'in_english': None, 'chart_color': None},
                        # 光熱費
-                       {'name': '光熱費', 'in_english': None, 'chart_color': None},
-                       {'name': '電気代', 'in_english': 'ElectricCharge', 'chart_color': 'rgb(255, 21, 51)'},
-                       {'name': 'ガス代', 'in_english': 'GasCharge', 'chart_color': 'rgb(255, 163, 0)'},
-                       {'name': '水道代', 'in_english': 'WaterCharge', 'chart_color': 'rgb(0, 106, 182)'},
+                       {'id': 200, 'name': '光熱費', 'in_english': None, 'chart_color': None},
+                       {'id': 201, 'name': '電気代', 'in_english': 'ElectricCharge', 'chart_color': 'rgb(255, 21, 51)'},
+                       {'id': 202, 'name': 'ガス代', 'in_english': 'GasCharge', 'chart_color': 'rgb(255, 163, 0)'},
+                       {'id': 203, 'name': '水道代', 'in_english': 'WaterCharge', 'chart_color': 'rgb(0, 106, 182)'},
                        # 食費
-                       {'name': '食費', 'in_english': None, 'chart_color': None},
-                       {'name': '食材費', 'in_english': None, 'chart_color': None},
-                       {'name': '外食費', 'in_english': None, 'chart_color': None},
+                       {'id': 300, 'name': '食費', 'in_english': None, 'chart_color': None},
+                       {'id': 301, 'name': '食材費', 'in_english': None, 'chart_color': None},
+                       {'id': 302, 'name': '外食費', 'in_english': None, 'chart_color': None},
                        # 日用品
-                       {'name': '日用品', 'in_english': None, 'chart_color': None},
-                       {'name': '日用品', 'in_english': None, 'chart_color': None},
-                       {'name': '洗剤類', 'in_english': None, 'chart_color': None},
+                       {'id': 400, 'name': '日用品', 'in_english': None, 'chart_color': None},
+                       {'id': 401, 'name': '日用品', 'in_english': None, 'chart_color': None},
+                       {'id': 402, 'name': '洗剤類', 'in_english': None, 'chart_color': None},
                        # 交通費
-                       {'name': '交通費', 'in_english': None, 'chart_color': None},
-                       {'name': 'タイムズ', 'in_english': None, 'chart_color': None},
-                       {'name': 'レンタカー', 'in_english': None, 'chart_color': None},
-                       {'name': 'ガソリン代', 'in_english': None, 'chart_color': None},
+                       {'id': 500, 'name': '交通費', 'in_english': None, 'chart_color': None},
+                       {'id': 501, 'name': 'タイムズ', 'in_english': None, 'chart_color': None},
+                       {'id': 502, 'name': 'レンタカー', 'in_english': None, 'chart_color': None},
+                       {'id': 503, 'name': 'ガソリン代', 'in_english': None, 'chart_color': None},
                    ]
                    )
     op.bulk_insert(category_paths,
                    [
                        # 固定費
-                       {'ancestor': 1, 'descendant': 1},  # 固定費
-                       {'ancestor': 1, 'descendant': 2},  # 家賃
-                       {'ancestor': 1, 'descendant': 3},  # 管理費
-                       {'ancestor': 1, 'descendant': 4},  # 手数料
-                       {'ancestor': 1, 'descendant': 5},  # 駐輪場
-                       {'ancestor': 1, 'descendant': 6},  # 手数料
+                       {'ancestor': 100, 'descendant': 100},  # 固定費
+                       {'ancestor': 100, 'descendant': 101},  # 家賃
+                       {'ancestor': 100, 'descendant': 102},  # 管理費
+                       {'ancestor': 100, 'descendant': 103},  # 手数料
+                       {'ancestor': 100, 'descendant': 104},  # 駐輪場
+                       {'ancestor': 100, 'descendant': 105},  # 手数料
                        # 光熱費
-                       {'ancestor': 7, 'descendant': 7},  # 光熱費
-                       {'ancestor': 7, 'descendant': 8},  # 電気代
-                       {'ancestor': 7, 'descendant': 9},  # ガス代
-                       {'ancestor': 7, 'descendant': 10},  # 水道代
+                       {'ancestor': 200, 'descendant': 200},  # 光熱費
+                       {'ancestor': 200, 'descendant': 201},  # 電気代
+                       {'ancestor': 200, 'descendant': 202},  # ガス代
+                       {'ancestor': 200, 'descendant': 203},  # 水道代
                        # 食費
-                       {'ancestor': 11, 'descendant': 11},  # 食費
-                       {'ancestor': 11, 'descendant': 12},  # 食材費
-                       {'ancestor': 11, 'descendant': 13},  # 外食費
+                       {'ancestor': 300, 'descendant': 300},  # 食費
+                       {'ancestor': 300, 'descendant': 301},  # 食材費
+                       {'ancestor': 300, 'descendant': 302},  # 外食費
                        # 日用品
-                       {'ancestor': 14, 'descendant': 14},  # 日用品
-                       {'ancestor': 14, 'descendant': 15},  # 日用品
-                       {'ancestor': 14, 'descendant': 16},  # 洗剤類
+                       {'ancestor': 400, 'descendant': 400},  # 日用品
+                       {'ancestor': 400, 'descendant': 401},  # 日用品
+                       {'ancestor': 400, 'descendant': 402},  # 洗剤類
                        # 交通費
-                       {'ancestor': 17, 'descendant': 17},  # 交通費
-                       {'ancestor': 17, 'descendant': 18},  # タイムズ
-                       {'ancestor': 17, 'descendant': 19},  # レンタカー
-                       {'ancestor': 17, 'descendant': 20},  # ガソリン代
+                       {'ancestor': 500, 'descendant': 500},  # 交通費
+                       {'ancestor': 500, 'descendant': 501},  # タイムズ
+                       {'ancestor': 500, 'descendant': 502},  # レンタカー
+                       {'ancestor': 500, 'descendant': 503},  # ガソリン代
                    ])
 
     # 同じwithの中だとエラーが発生したので、処理を分けた
